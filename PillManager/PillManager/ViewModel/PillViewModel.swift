feba6868defd64b9b2c118b8d3354d255b283fef
신동규 <donggyu9410@gmail.com>
2022-03-20 12:21:09 +0900

    feat: Work local notification feature

--- PillManager/PillManager/ViewModel/PillViewModel.swift
+++ PillManager/PillManager/ViewModel/PillViewModel.swift
@@ -29,14 +29,13 @@ class PillViewModel {
         self.pillDataCenter = pillDataCenter
         bind()
         fetchPillDate()
-        requestSendNoti()
-        testNoti()
+        requestSendNotification()
     }
     
     func buttonTapped() {
         if status == .not_yet {
             self.status = .loading
-            self.pillDataCenter.addPill { [weak self] error in
+            self.pillDataCenter.savePill { [weak self] error in
                 self?.error = error
                 self?.fetchPillDate()
             }
@@ -59,7 +58,7 @@ class PillViewModel {
     
     private func fetchPillDate() {
         status = .loading
-        pillDataCenter.getPillDate { [weak self] result in
+        pillDataCenter.fetchPillDate { [weak self] result in
             switch result {
             case .success(let date):
                 self?.pillDate = date
@@ -74,14 +73,13 @@ class PillViewModel {
     }
     
     // 알림 전송
-    private func requestSendNoti() {
+    private func requestSendNotification() {
         let notiContent = UNMutableNotificationContent()
         notiContent.title = "Night"
         notiContent.body = "Forgot daily pills?"
         notiContent.badge = .init(value: 1)
 //        notiContent.userInfo = ["targetScene": "splash"] // 푸시 받을때 오는 데이터
 
-        // 알림이 trigger되는 시간 설정
         var dateComponents = DateComponents()
         
         dateComponents.hour = 22
@@ -94,6 +92,12 @@ class PillViewModel {
             content: notiContent,
             trigger: trigger
         )
+        
+        // 오늘 약을 이미 먹은 경우에는 종료
+        if let date = pillDataCenter.fetchPillDateLocal() {
+            let now = Date()
+            if isSameDay(date1: date, date2: now) { return }
+        }
 
         notificationCenter.add(request) { (error) in
             if let error = error {
@@ -101,27 +105,5 @@ class PillViewModel {
             }
         }
 
-    }
-    
-    private func testNoti() {
-        let notiContent = UNMutableNotificationContent()
-        notiContent.title = "Night"
-        notiContent.body = "Forgot daily pills?"
-        notiContent.badge = .init(value: 1)
-        
-        let trigger: UNTimeIntervalNotificationTrigger = .init(timeInterval: 10, repeats: false)
-        
-        let request = UNNotificationRequest(
-            identifier: UUID().uuidString,
-            content: notiContent,
-            trigger: trigger
-        )
-
-        notificationCenter.add(request) { (error) in
-            print("DEBUG: 여기 오낭?")
-            if let error = error {
-                print("DEBUG: error \(error.localizedDescription)")
-            }
-        }
     }
 }
